759937a6e502463aac3035562922bae6ec6b71b9
0x1 <kevin.francart3@gmail.com>
2026-08-01 13:49:48 +0200

    fix(ffi): fix various FFI implementations

--- src/Data/Date.go
+++ src/Data/Date.go
@@ -9,9 +9,9 @@ func createDate(y, m, d int) time.Time {
 	return time.Date(y, time.Month(m+1), d, 0, 0, 0, 0, time.UTC)
 }
 
-func CanonicalDateImpl(ctor func(int) func(int) func(int) interface{}, y int, m int, d int) interface{} {
+func CanonicalDateImpl(ctor interface{}, y int, m int, d int) interface{} {
 	date := createDate(y, m-1, d)
-	return ctor(date.Year())(int(date.Month()))(date.Day())
+	return ctor.(func(interface{}) interface{})(date.Year()).(func(interface{}) interface{})(int(date.Month())).(func(interface{}) interface{})(date.Day())
 }
 
 func CalcWeekday(y int, m int, d int) int {
@@ -24,5 +24,3 @@ func CalcDiff(y1 int, m1 int, d1 int, y2 int, m2 int, d2 int) float64 {
 	dt2 := createDate(y2, m2-1, d2)
 	return float64(dt1.UnixMilli() - dt2.UnixMilli())
 }
-
-func CalcDiff(y int, m int, d int) int { return 0 }
